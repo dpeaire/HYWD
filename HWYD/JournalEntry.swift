d074855c4f2ded37fcf417940b522c0f7acd1f6d
Dustin Peaire <dustinpeaire@dustins-macbook-pro.local>
2017-04-09 15:56:06 -0400

    Added message bubble creators

--- HWYD/JournalEntry.swift
+++ HWYD/JournalEntry.swift
@@ -6,10 +6,46 @@
 //  Copyright © 2017 Dustin Peaire. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import JSQMessagesViewController
 
 class JournalEntry: JSQMessagesViewController {
-    
-}
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.senderId = "Dustin"
+        self.senderDisplayName = "Dustin"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: -UI
+    
+    private func setupOutgoingBubble() -> JSQMessagesBubbleImage{
+        let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+        return bubbleImageFactory!.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+        
+    }
+    
+    private func setupIncomingBubble() -> JSQMessagesBubbleImage {
+        let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+        return bubbleImageFactory!.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
