864873b5d5c38e21744da25e2e1c6e0cfaae6e46
Dustin Peaire <dustinpeaire@dustins-macbook-pro.local>
2017-04-09 19:40:31 -0400

    Initiated UICollectionView

---
+++ HWYD/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  HWYD
+//
+//  Created by Dustin Peaire on 4/9/17.
+//  Copyright © 2017 Dustin Peaire. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
